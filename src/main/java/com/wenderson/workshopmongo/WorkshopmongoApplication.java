c4f92050a304ab47abc68d955a4012091c7ccbbc
wendersonfarias <wendersonpfarias@gmail.com>
2022-05-20 11:18:43 -0400

    Primeiro commit - projeto criado

---
+++ src/main/java/com/wenderson/workshopmongo/WorkshopmongoApplication.java
@@ -0,0 +1,15 @@
+package com.wenderson.workshopmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopmongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopmongoApplication.class, args)
+		
+		;
+	}
+
+}
