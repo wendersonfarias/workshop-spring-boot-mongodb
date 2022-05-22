80637db5093e19254103d4c21387cd73c6e687b3
Wendersonfarias <wendersonpfarias@gmail.com>
2022-05-21 21:14:16 -0400

    Obtendo um post por id

---
+++ src/main/java/com/wenderson/workshopmongo/services/PostService.java
@@ -0,0 +1,23 @@
+package com.wenderson.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.wenderson.workshopmongo.domain.Post;
+import com.wenderson.workshopmongo.repository.PostRepository;
+import com.wenderson.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+
+}
